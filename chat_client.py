c9026d89e7866c01617e02a6e84f655dde8fc158
Chastener <gedimin.vitalik@gmail.com>
2018-11-30 20:56:52 +0300

    Start commit
    There is Socket_server and client for it.
    Database_methods for working with mysqldatabase and local_settings example file that is needed for connection between server and database.

---
+++ chat_client.py
@@ -0,0 +1,41 @@
+""" The chat client """
+
+import sys
+import select
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+COMMANDS = {'/h', '/l', '/x', '/u', '/t', }
+
+if len(sys.argv) != 3:
+    IP_address = '0.0.0.0'
+    Port = 5050
+else:
+    IP_address = str(sys.argv[1])
+    Port = int(sys.argv[2])
+
+server.connect((IP_address, Port))
+chat = True
+
+while chat:
+    sockets_list = [sys.stdin, server]
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+
+    for socket in read_sockets:
+        if socket == server:
+            message = socket.recv(2048).decode()
+            if not message:  # server closed
+                chat = False
+                print('Server closed. Bye.')
+                break
+            sys.stdout.write(message)
+            sys.stdout.write('\n ')
+            sys.stdout.flush()
+            if message.strip().startswith('Bye.'):
+                chat = False
+                break
+        else:
+            message = sys.stdin.readline()
+            if not message =='\n':
+                server.send(message.encode())
+server.close()
---
+++ local_settings.py
@@ -0,0 +1,6 @@
+import mysql.connector
+
+connector = mysql.connector.connect(user='vitali',
+                                    password='1',
+                                    host='localhost',
+                                    database='chat')
